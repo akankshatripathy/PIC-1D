1b6a23a2c5839e491ac4878210776cbc9c318c09
Akanksha Tripathy <atripath@sunfire02.pppl.gov>
2020-04-20 16:11:22 -0400

    updated to newest code

--- pic1d.py
+++ pic1d.py
@@ -1,7 +1,6 @@
 #!/bin/bash python
 
-#Do 1D SOL flux-tube, to test out whether NOT including all sources in the
-#electron subcycling push will make a difference
+
 #(Currently includes the wall sink, but not neutrals, etc.)
 
 import numpy as np
@@ -15,7 +14,7 @@ charge = 1.609e-19
 ne = 1e19 #electron density (const along SOL flux tube)
 Te = 39 #90 #electron temperature (const along SOL flux tube)
 potSheath = 3.*Te
-Np =int(1e6) #number of markers
+Np =int(1e3) #number of markers
 Nsubcycle = 100
 Ntimes = 10
 sml_dt = 0.002
@@ -24,6 +23,8 @@ sml_ev2j = charge
 sml_j2ev = 1./charge
 file_grid = 'grid.npz'
 file_neutrals = 'neutrals.npz'
+t = 0
+
 
 def interpolate_fieldLineFollow(Lstart,phiEnd,Binterp):
 
@@ -60,15 +61,29 @@ def create_1d_grid():
 
     Binterp = LinearNDInterpolator(RZ, Bgrid, fill_value = np.inf)
     ind = wall_nodes[2]
-    inds = range(wall_nodes[2],wall_nodes[2]+180)
+    #inds = range(wall_nodes[2],wall_nodes[2]+180)
+    inds = range(wall_nodes[2],wall_nodes[3]+1)
     Lstart = np.array([RZ[ind,0],RZ[ind,1],0.])
-    phiEnd = -9*np.pi
+    phiEnd = -25*np.pi
     Lout = interpolate_fieldLineFollow(Lstart,phiEnd,Binterp)
-    LparallelTot = np.hstack((0.,np.cumsum(np.sqrt(np.diff(Lout[:,0]*np.cos(-Lout[:,2]))**2. + \
+    LparallelOut = np.hstack((0.,np.cumsum(np.sqrt(np.diff(Lout[:,0]*np.cos(-Lout[:,2]))**2. + \
                             np.diff(Lout[:,0]*np.sin(-Lout[:,2]))**2. + \
-                            np.diff(Lout[:,1])**2. ) )))
-beforeMid = np.where(Lout[:,1]>0.)[0][0]
-    x = np.interp(RZ[inds,1],Lout[0:beforeMid,1],LparallelTot[0:beforeMid])
+    
+                        np.diff(Lout[:,1])**2. ) )))
+    print(LparallelOut)
+    #beforeMid = np.where(Lout[:,1]>0.)[0][0]
+    lastind = np.where(np.isnan(LparallelOut))[0][0]
+    #x = np.interp(RZ[inds,1],Lout[0:lastind,1],LparallelOut[0:lastind])
+    Ltheta = np.cumsum(np.sqrt(np.diff(RZ[inds,0])**2 + np.diff(RZ[inds,1])**2))
+    Ltheta = np.hstack((0.,Ltheta))
+    Lthetaout = np.cumsum(np.sqrt(np.diff(Lout[:,0])**2 + np.diff(Lout[:,1])**2))
+    Lthetaout = np.hstack((0.,Lthetaout))
+    x = np.interp(Ltheta, Lthetaout[0:lastind], LparallelOut[0:lastind])
+    plt.figure()
+    #plt.plot(Lout[:,0],Lout[:,1], '.')
+    #plt.plot(RZ[wall_nodes, 0],RZ[wall_nodes, 1], '.')
+    print(Ltheta)
+    plt.plot(Lthetaout,LparallelOut)
     return x
 
 def load_markers(x,mass,ne,Te,Np,marker_den):
@@ -98,14 +113,8 @@ def load_markers(x,mass,ne,Te,Np,marker_den):
     g = np.empty((Np,))
 
     v[indslt] = A[indslt]/C
-indsge = np.where(A >= C*va)[0]
-
-    v = np.empty((Np,))
-    g = np.empty((Np,))
-
-    v[indslt] = A[indslt]/C
     v[indsge] = va - vwidth*np.log(1.+(va-A[indsge]/C)/vwidth)
-
+    
     g[indslt] = C
     g[indsge] = C*np.exp(-(v[indsge]-va)/vwidth)
 
@@ -119,7 +128,7 @@ indsge = np.where(A >= C*va)[0]
     f0 = ne*maxwell_norm*np.exp(-0.5*(vp/vth)**2.)
     w1 = sml_initial_deltaf_noise*2.*(np.random.rand(Np)-0.5)
     w2 = sml_initial_deltaf_noise*2.*(np.random.rand(Np)-0.5)
-     #w2 = w1.copy() #XGC uses same w1 and w2 initial. Can cause issues I think
+    #w2 = w1.copy() #XGC uses same w1 and w2 initial. Can cause issues I think
     return xp,vp,w0,f0,w1,w2
 
 def load_neutrals(file_neutrals):
@@ -139,6 +148,18 @@ def charge_update(xp,vp,w1,w2,f0):
     w1new = w1 + (w2new-w2)
     return w1new,w2new,f0new
 
+def calc_f(x,xp,w0,w1):
+    indx = np.interp(xp,x,np.arange(x.size))
+    wp = indx - np.floor(indx)
+    temp = Te*sml_ev2j
+    v = np.linspace(-4*np.sqrt(temp/mass),4*np.sqrt(temp/mass),32)
+    f = np.empty((x.size, v.size))
+    for ip in range(xp.size):
+        indv = np.argmin(np.abs(vp[ip] - v))
+        f[np.floor(indx[ip]).astype(int), indv] += w0[ip]*w1[ip]
+        f[np.ceil(indx[ip]).astype(int), indv] += w0[ip]*w1[ip]
+    return f
+
 def calc_eden(x,xp,w0,w1):
     indx = np.interp(xp,x,np.arange(x.size))
     wp = indx - np.floor(indx)
@@ -148,45 +169,90 @@ def calc_eden(x,xp,w0,w1):
     eden[0:-1] += cnts1
     eden[1:]   += cnts2
     return eden
+def f_sourcegrid(f,xp,vp,w0,w1,n_n):
+    dfel = n_n[:,np.newaxis]*(0.8e-8*np.sqrt(Te)*np.exp(-13.56/Te)*(1./(1+0.01*Te))*1e-6)*dt*f
+    w1new =  meshtoparticle(dfel,x,xp,vp,w0,w1)
+    return w1new
+
+def meshtoparticle(df,x,xp,vp,w0,w1):
+    temp = Te*sml_ev2j
+    v = np.linspace(-4*np.sqrt(temp/mass),4*np.sqrt(temp/mass),32)
+    dv = v[1]-v[0]
+    w1new = w1.copy()
+    indx = np.interp(xp,x,np.arange(x.size))
+    wp = indx - np.floor(indx)
+    wpdens = 0*df.copy()
+    for i in range(v.size):
+        vinds = np.where ((vp > v[i] - dv/2) & (vp <= v[i] + dv/2))[0]
+        for ix in range(x.size):
+            if ix == 0:
+                xstart = x[0]
+                xend = x[1]
+            elif ix == x.size-1:
+                xstart = x[-2]
+                xend = x[-1]
+            else:
+                xstart = x[ix-1]
+                xend = x[ix+1]
+            xinds = np.where((xp > xstart) & (xp <= xend))[0]
+            inds = np.intersect1d(xinds,vinds)
+            if inds.size == 0:continue
+            wpden = np.sum(wp[inds])
+            dfep = (wp[inds]/wpden)*(df[ix,i])
+            w1new[inds] += dfep/w0[inds]
+            wpdens[ix,i] = wpden
+    #return w1new,wpdens
+    return w1new
 
 def pushe(xp,vp,w1,w2):
- #subcycle electrons (100 subcycle steps)
+    #subcycle electrons (100 subcycle steps)
+    #print(dt)
     xpold= xp.copy()
     vpold = vp.copy()
     vpnew = vp.copy()
     w1old = w1.copy()
     for itn in range(Nsubcycle):
-        xpnew = xpold + vpold*dt*itn/float(Nsubcycle)
+        xpnew = xpold + vpold*dt/float(Nsubcycle)
 
         #check for particles that went through sheath. THIS IS AN EFFECTIVE SOURCE
-        outinds = np.where(xpnew<0)[0]
+        outinds = np.where((xpnew<0) | (xpnew>x[-1]))[0]
         if outinds.size>0:
             xpnew[outinds] = xpold[outinds] #put particle to previous position
             vpnew[outinds] = -vpold[outinds] #reflect velocity of particle
-
+            #print("sanity check")
             En = 0.5*mass*vpnew[outinds]**2.*sml_j2ev
             highEinds = np.where(En>potSheath)[0]
             if highEinds.size>0:
                 w1[outinds[highEinds]] = -1. + w2[outinds[highEinds]]
 
         #check for particles that went past LFS midplane
-        lostinds = np.where(xpnew>x[-1])[0]
-        if lostinds.size>0:
-            w1[lostinds] = 0.
-            w2[lostinds] = 0.
-            xpnew[lostinds] = xpold[lostinds] #just gather at the boundary
+        #lostinds = np.where(xpnew>x[-1])[0]
+        #if lostinds.size>0:
+            #w1[lostinds] = 0.
+            #w2[lostinds] = 0.
+            #xpnew[lostinds] = xpold[lostinds] #just gather at the boundary
+            #print(itn, w1.min(),np.sum(xpnew > (x[-1] - 0.1))/float(xpnew.size))
+            #print(2*x[-1]/(np.sqrt(Te*charge/mass)))
 
         xpold = xpnew.copy()
-
+ 
+        vpold = vpnew.copy()
+	
     return (xpnew,vpnew,w1,w2)
-    def f_source(n_n,Te,dt,w0,f0,w1):
+
+def f_source(n_n,Te,dt,w0,f0,w1):
     #for now, just the cold electron neutral piece
-    #this is not the same as XGC, doing directly to the particle instead of
+    #this is not the same as XGC, doing directly to the particle instead of 
     #going through the grid first
     n_np = np.interp(xp,x,n_n)
     w1new = w1 + n_np*(0.8e-8*np.sqrt(Te)*np.exp(-13.56/Te)*(1./(1+0.01*Te))*1e-6)*dt*f0/w0
-    return w1new
+    return w1new 
 
+def plot(xp,w1,it):
+     plt.figure()
+     plt.plot(xp, w1, '.')
+     plt.savefig('w1_t' + str(it).zfill(3) + '.png')
+	
 #def main():
 if __name__=="__main__":
     #first, load the mesh grid. x=0 is LFS divertor, x[-1] is LFS midplane
@@ -202,23 +268,64 @@ if __name__=="__main__":
     n_n = load_neutrals(file_neutrals)
     #this is ordered from LFS divertor to HFS divertor. Interpolate by index
     #onto x grid
-    n_n = np.interp(np.arange(x.size),np.arange(n_n.size),n_n)
 
     #now, do the main time loop
     dt = sml_dt*7.9e-5 #put in units of seconds
 
     eden = np.empty((x.size,Ntimes))
     for it in range(Ntimes):
+	
         print('Step %d' % it)
-        w1,w2,_  = charge_update(xp,vp,w1,w2,f0)
+        #w1,w2,_  = charge_update(xp,vp,w1,w2,f0)
 
-        eden[:,it] = calc_eden(x,xp,w0,w1)
-        xp,vp,w1,w2 = pushe(xp,vp,w1,w2)
+        #eden[:,it] = calc_eden(x,xp,w0,w1)
 
-        #w1 = f_source(n_n,Te,dt,w0,f0,w1)
+        #plot(xp,w1,it)
 
+        #xp,vp,w1,w2 = pushe(xp,vp,w1,w2)
 
+        #foriginal = calc_f(x,xp,w0,w1)
 
+        #fbig = np.zeros(f.shape)
+        #fbig[:,20] = 1e12
 
+        #print(w1.min(),w1.max())
 
+        #w1big,wpdens = meshtoparticle(fbig,x, xp,vp,w0,w1)
 
+        #print(w1big.min(),w1big.max())
+
+        #ftest = calc_f(x,xp,w0,w1big)-foriginal
+
+        #plt.figure()
+
+        #plt.contourf(ftest,cmap = "Reds")
+
+        #plt.title("ftest")
+ 
+        #plt.colorbar()
+
+        #plt.figure()
+
+        #plt.contourf(fbig,cmap = "Reds")
+  
+        #plt.title("fbig")
+
+        #plt.colorbar()
+
+        #plt.xlim([15,35])
+
+        #plt.ylim([0,100])
+
+        #w1 = f_source(n_n,Te,dt,w0,f,w1)
+        #fanalytical = get_f0(x,v)
+        #fparticle = calc_f(
+        #ftotal = fanalytical + fparticle
+
+        w1 = f_sourcegrid(ftotal,xp,vp,w0,w1,n_n)
+ 
+        if np.all(w1 < 0):
+ 
+            print("all w1 negative at time step %d" % it)  
+
+    plot(xp, w1, it)    
